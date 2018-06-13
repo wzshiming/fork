1ca6dcb41ec5bcf001b0f26840fd4b9d711c099d
wzshiming <wzshiming@foxmail.com>
2018-06-13 16:33:19 +0800

    update Comments

--- fork.go
+++ fork.go
@@ -1,4 +1,4 @@
-package fork // import "gopkg.in/wzshiming/fork.v2"
+package fork
 
 import (
 	"sync"
@@ -7,9 +7,9 @@ import (
 var none = struct{}{}
 
 type Fork struct {
-	buf chan func()    // 缓冲闭包
-	max chan struct{}  // 最大线程
-	wg  sync.WaitGroup // 等待组
+	buf chan func()    // pending
+	max chan struct{}  // maximum fork
+	wg  sync.WaitGroup // wait group
 }
 
 func NewForkBuf(max int, buf int) *Fork {
@@ -23,14 +23,14 @@ func NewFork(max int) *Fork {
 	return NewForkBuf(max, 1)
 }
 
-// 执行任务的 长度 最大 只能是线程数加缓冲数 其他的阻塞看不到
+// Len returns the size of the wait and the current fork
 func (fo *Fork) Len() int {
 	return len(fo.buf) + len(fo.max)
 }
 
-// 加入闭包 可以的话立即执行
+// Push trying to perform
 func (fo *Fork) Push(f func()) {
-	// 如果未达到最大线程则开启新线程执行 缓冲已满就在这里阻塞
+	// If the maximum fork is not reached, the new fork execution buffer is full and blocked here
 	fo.wg.Add(1)
 	select {
 	case fo.max <- none:
@@ -41,7 +41,7 @@ func (fo *Fork) Push(f func()) {
 	return
 }
 
-// 把当前线程加入 线程执行队列
+// fork a new fork
 func (fo *Fork) fork(f0 func()) bool {
 	if f0 != nil {
 		f0()
@@ -58,7 +58,7 @@ func (fo *Fork) fork(f0 func()) bool {
 	return true
 }
 
-// 等待所有线程结束在返回
+// Join wait blocks until the WaitGroup counter is zero.
 func (fo *Fork) Join() {
 	fo.wg.Wait()
 	return
